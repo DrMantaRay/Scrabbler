930c6aec23de337c08938efc47f3ce15e1922621
DrMantaRay <patrick.chen64@gmail.com>
2017-08-06 20:37:27 -0700

    finished word finder

--- Scrabbler.py
+++ Scrabbler.py
@@ -4,8 +4,24 @@ import pickle
 from itertools import combinations
 
 
-class Scrabbler:
+class Board:
+    def __init__(self, width, height):
+        self.height = 15
+        self.width = 15
+        self.board_array = BoardTile[15][15]
 
+
+class BoardTile:
+    def __init__(self, x , y):
+        self.char = ''
+        self.type = 0
+        self.x = x
+        self.y = y
+
+
+class WordFinder:
+
+    # initializes various useful variables
     def __init__(self):
         self.hand_letters = ""
         self.board_letters = ""
@@ -13,19 +29,42 @@ class Scrabbler:
         self.all_letters = ""
         self.word_list = set()
 
+    # permutes the entered word and finds possible words
     def permute(self):
+        alpha_list = "abcdefghijklmnopqrstuvwxyz"
         self.word_list = set()
         self.all_letters = self.hand_letters + self.board_letters
         self.all_letters = sorted(self.all_letters)
         for i in range(1, len(self.all_letters) + 1):
             for word in combinations(self.all_letters, i):
-                if
-                if ''.join(word) in self.wordDict:
+                if '/' in word:
+                    num_blanks = word.count('/')
+                    for filler in combinations(num_blanks*alpha_list, num_blanks):
+                        filled_word = ''.join(sorted(self.blank_replace(''.join(word), filler)))
+                        if filled_word in self.wordDict:
+                            for real_word in self.wordDict[filled_word]:
+                                if self.board_letters in real_word:
+                                    self.word_list.add(real_word)
+
+                elif ''.join(word) in self.wordDict:
                     for real_word in self.wordDict[''.join(word)]:
                         if self.board_letters in real_word:
                             self.word_list.add(real_word)
 
-    def init_scrabbler(self):
+    # helper function that replaces blanks with a substring
+    def blank_replace(self, string, substring):
+        return_string = []
+        i = 0
+        for char in string:
+            if char == '/':
+                return_string.append(substring[i])
+                i += 1
+            else:
+                return_string.append(char)
+        return ''.join(return_string)
+
+    # the ui for word finder
+    def init(self):
         print("Welcome to Scrabbler!")
         print("Given an arbitrary set of letters to be played and a sequence of letters on the board,")
         print("Scrabbler search for all possible word combinations according to the dictionary of your choice.")
@@ -45,16 +84,16 @@ class Scrabbler:
                 dictionary = input("The only valid inputs are 1,2,3. Try again:\n")
 
         while True:
-            self.hand_letters = input("Input the letters in your hand, \
-                                Use a forward slash for blanks '/'. Ex: dei/bp:\n")
+            self.hand_letters = \
+                input("Input the letters in your hand,Use a forward slash for blanks '/'. Example: dei/bp...\n")
             print("Input the letter sequence you wish to put your tiles around. Leave blank if you just want to find ")
-            self.board_letters = input("word combinations from the letters in your hand:\n")
+            self.board_letters = input("word combinations from the letters in your hand...\n")
             self.permute()
             print("Word List:")
-            for word in self.word_list:
+            for word in sorted(self.word_list):
                 print(word)
-            input()
+            input("Press enter to start over")
 
 if __name__ == '__main__':
-    scrabbler = Scrabbler()
-    scrabbler.init_scrabbler()
+    word_finder = WordFinder()
+    word_finder.init()
---
+++ settings.py
@@ -0,0 +1,5 @@
+# settings for scrabble
+
+class
+height = 15
+width = 15
